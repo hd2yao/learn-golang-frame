c05dd77deccdbe8c88c7d7feab1656c3af35a5a3
hd2yao <1054290355@qq.com>
2024-08-07 15:01:33 +0800

    feat: 创建实例的方法

--- ants/pool.go
+++ ants/pool.go
@@ -31,3 +31,27 @@ type Pool struct {
     // once is used to ensure that the pool shutdown is performed only once
     once sync.Once
 }
+
+// NewPool 创建一个实例
+func NewPool(size int) (*Pool, error) {
+    return NewTimingPool(size, DefaultCleanIntervalTime)
+}
+
+// NewTimingPool 创建一个带有自定义定时任务的实例
+func NewTimingPool(size, expiry int) (*Pool, error) {
+    if size <= 0 {
+        return nil, ErrInvalidPoolSize
+    }
+    if expiry <= 0 {
+        return nil, ErrInvalidPoolExpiry
+    }
+    p := &Pool{
+        capacity:       int32(size),
+        release:        make(chan sig, 1),
+        expiryDuration: time.Duration(expiry) * time.Second,
+    }
+
+    // 启动定期清理过期 worker 任务，独立 goroutine 运行，节省系统资源
+    p.monitorAndClear()
+    return p, nil
+}
